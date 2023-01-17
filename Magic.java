84b52c2c5baf8c8735ded37a70393ca7591db944
unknown <arturodiazmay@gmail.com>
2023-01-16 19:13:04 -0600

    Created the Last 3 Steps + Added Println Statement

--- Magic.java
+++ Magic.java
@@ -1,10 +1,13 @@
 public class Magic {
 	public static void main(String[] args) {
-    int myNumber = 7;
-    // myNumber is the original number
+    int myNumber = 8;
+    // myNumber is the original number but can be any number
     int stepOne = myNumber * myNumber;
     int stepTwo = stepOne + myNumber;
     int stepThree = stepTwo / myNumber;
-    
+    int stepFour = stepThree + 17;
+    int stepFive = stepFour - myNumber;
+    int stepSix = stepFive / 6;
+    System.out.println(stepSix);
 	}
 }
