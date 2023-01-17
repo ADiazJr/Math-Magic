1bf1f84179ee0960df1f44a78d5b694252fdc3d7
unknown <arturodiazmay@gmail.com>
2023-01-16 19:18:27 -0600

    Removed Step Variables and Replaced with a single magicNumber Variable

--- Magic.java
+++ Magic.java
@@ -1,13 +1,13 @@
 public class Magic {
 	public static void main(String[] args) {
-    int myNumber = 8;
-    // myNumber is the original number but can be any number
-    int stepOne = myNumber * myNumber;
-    int stepTwo = stepOne + myNumber;
-    int stepThree = stepTwo / myNumber;
-    int stepFour = stepThree + 17;
-    int stepFive = stepFour - myNumber;
-    int stepSix = stepFive / 6;
-    System.out.println(stepSix);
+    int myNumber = 2;
+    // myNumber is the original number
+    int magicNumber = myNumber * myNumber;
+    magicNumber = magicNumber + myNumber;
+    magicNumber = magicNumber / myNumber;
+    magicNumber = magicNumber + 17;
+    magicNumber = magicNumber - myNumber;
+    magicNumber = magicNumber / 6;
+    System.out.println(magicNumber);
 	}
 }
