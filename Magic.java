2d7159ad93f78bac792408b9a8acd2c30e3c4fa2
unknown <arturodiazmay@gmail.com>
2023-01-16 19:10:11 -0600

    Created First Four Variables With Math Functions

---
+++ Magic.java
@@ -0,0 +1,10 @@
+public class Magic {
+	public static void main(String[] args) {
+    int myNumber = 7;
+    // myNumber is the original number
+    int stepOne = myNumber * myNumber;
+    int stepTwo = stepOne + myNumber;
+    int stepThree = stepTwo / myNumber;
+    
+	}
+}
